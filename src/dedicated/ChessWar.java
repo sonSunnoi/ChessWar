51bfb73fe6bec659873bba8e2d69f8d06594d8c7
Jirasin Chaimahawong <whoknowtrueofearth@gmail.com>
2019-04-12 18:50:00 +0700

    Create dedicate application and runnable

---
+++ src/dedicated/ChessWar.java
@@ -0,0 +1,16 @@
+package dedicated;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ChessWar extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ src/dedicated/ChessWarArena.java
@@ -0,0 +1,11 @@
+package dedicated;
+
+public class ChessWarArena implements Runnable {
+
+    @Override
+    public void run() {
+        while()
+    }
+
+    //Can make a whole game a thread?
+}

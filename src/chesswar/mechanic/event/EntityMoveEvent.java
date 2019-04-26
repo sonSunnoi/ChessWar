ba300d869703f70f00bcf5c424effd089c1a5a18
Jirasin Chaimahawong <whoknowtrueofearth@gmail.com>
2019-04-27 02:12:56 +0700

    Update constructor

--- src/chesswar/mechanic/event/EntityMoveEvent.java
+++ src/chesswar/mechanic/event/EntityMoveEvent.java
@@ -1,12 +1,17 @@
 package chesswar.mechanic.event;
 
 import chesswar.entity.Chessman;
+import chesswar.mechanic.board.Field;
 
 public class EntityMoveEvent implements Event {
 
     private Chessman chessman;
+    private Field before;
+    private Field after;
 
-    public EntityMoveEvent(Chessman chessman) {
+    public EntityMoveEvent(Chessman chessman, Field before, Field after) {
         this.chessman = chessman;
+        this.before = before;
+        this.after = after;
     }
 }
